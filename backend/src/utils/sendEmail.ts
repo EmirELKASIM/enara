8571c85e4e840f49f950b86a02c950401f1b1058
Emir ELKASIM <amirsy005@gmail.com>
2026-03-09 19:48:59 +0300

    updat sendMethod

--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -4,9 +4,15 @@ import jwt from "jsonwebtoken";
 export const sendEmail = async (to: string, subject: string, html: string) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // يجب أن تكون true للمنفذ 465
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS, // App Password
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false, // هذا السطر ضروري جداً لتجاوز الـ Timeout في Render
     },
   });
 
@@ -18,14 +24,10 @@ export const sendEmail = async (to: string, subject: string, html: string) => {
   });
 };
 
-
 export const sendVerificationEmail = async (user: any) => {
-
-  const token = jwt.sign(
-    { userId: user._id },
-    process.env.JWT_SECRET!,
-    { expiresIn: "1d" }
-  );
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, {
+    expiresIn: "1d",
+  });
 
   const link = `${process.env.FRONTEND_URL}/user/verify-email/${token}`;
 
