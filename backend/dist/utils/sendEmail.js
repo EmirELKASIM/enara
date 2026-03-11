f33083a784d2c3092838616125ae81b516b09a29
Emir ELKASIM <amirsy005@gmail.com>
2026-03-11 15:46:31 +0300

    update sendEmail function

--- backend/dist/utils/sendEmail.js
+++ backend/dist/utils/sendEmail.js
@@ -4,43 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sendVerificationEmail = exports.sendEmail = void 0;
-// utils/sendEmail.ts
-const nodemailer_1 = __importDefault(require("nodemailer"));
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const resend_1 = require("resend");
+const resend = new resend_1.Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (to, subject, html) => {
-    const transporter = nodemailer_1.default.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-    });
-    await transporter.verify();
-    console.log("SMTP Connected");
-    await transporter.sendMail({
-        from: `"Support" <${process.env.EMAIL_USER}>`,
-        to,
-        subject,
-        html,
-    });
+    try {
+        const response = await resend.emails.send({
+            from: `Support <${process.env.EMAIL_USER}>`,
+            to: to,
+            subject: subject,
+            html: html,
+        });
+        console.log("Email sent:", response);
+    }
+    catch (error) {
+        console.error("Error sending email:", error);
+    }
 };
 exports.sendEmail = sendEmail;
-// export const sendVerificationEmail = async (user: any) => {
-//   const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, {
-//     expiresIn: "1d",
-//   });
-//   const link = `${process.env.FRONTEND_URL}/user/verify-email/${token}`;
-//   const html = `
-//     <h2>Email Verification</h2>
-//     <p>Please click the link below to verify your email:</p>
-//     <a href="${link}">Verify Email</a>
-//   `;
-//   await sendEmail(user.email, "Verify Your Email", html);
-// };
-const resend = new resend_1.Resend(process.env.RESEND_API_KEY);
 const sendVerificationEmail = async (user) => {
     const token = jsonwebtoken_1.default.sign({ userId: user._id }, process.env.JWT_SECRET, {
         expiresIn: "1d",
@@ -51,16 +32,12 @@ const sendVerificationEmail = async (user) => {
     <p>Please click the link below to verify your email:</p>
     <a href="${link}">Verify Email</a>
   `;
-    const resault = await resend.emails.send({
+    await resend.emails.send({
         from: `Enara <${process.env.EMAIL_USER}>`,
         to: user.email,
         subject: "Verify Your Email",
         html: html,
     });
-    console.log(resault);
-    console.log(process.env.RESEND_API_KEY);
-    console.log(process.env.FRONTEND_URL);
-    console.log(link);
 };
 exports.sendVerificationEmail = sendVerificationEmail;
 //# sourceMappingURL=sendEmail.js.map
--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -1,49 +1,23 @@
 // utils/sendEmail.ts
 import nodemailer from "nodemailer";
 import jwt from "jsonwebtoken";
-import sgMail from "@sendgrid/mail";
-
 import { Resend } from "resend";
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  try {
+    const response = await resend.emails.send({
+      from: `Support <${process.env.EMAIL_USER}>`,
+      to: to,
+      subject: subject,
+      html: html,
+    });
 
-  await transporter.verify();
-  console.log("SMTP Connected");
-
-  await transporter.sendMail({
-    from: `"Support" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+    console.log("Email sent:", response);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
 };
-
-// export const sendVerificationEmail = async (user: any) => {
-//   const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, {
-//     expiresIn: "1d",
-//   });
-
-//   const link = `${process.env.FRONTEND_URL}/user/verify-email/${token}`;
-
-//   const html = `
-//     <h2>Email Verification</h2>
-//     <p>Please click the link below to verify your email:</p>
-//     <a href="${link}">Verify Email</a>
-//   `;
-
-//   await sendEmail(user.email, "Verify Your Email", html);
-// };
-
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (user: any) => {
   const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, {
@@ -58,14 +32,10 @@ export const sendVerificationEmail = async (user: any) => {
     <a href="${link}">Verify Email</a>
   `;
 
-  const resault = await resend.emails.send({
+  await resend.emails.send({
     from: `Enara <${process.env.EMAIL_USER}>`,
     to: user.email,
     subject: "Verify Your Email",
     html: html,
   });
-  console.log(resault);
-  console.log(process.env.RESEND_API_KEY);
-  console.log(process.env.FRONTEND_URL);
-  console.log(link);
 };
